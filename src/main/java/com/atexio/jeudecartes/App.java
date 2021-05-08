f3eb886a904511e09c01d5fa9a441c10af588e76
Kmeneouali <khalledmeneouali89@gmail.com>
2021-05-08 04:21:38 +0200

    first commit

---
+++ src/main/java/com/atexio/jeudecartes/App.java
@@ -0,0 +1,13 @@
+package com.atexio.jeudecartes;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
