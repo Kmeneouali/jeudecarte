49c5de27a300d825ce68657cd3055fa872499eea
Kmeneouali <khalledmeneouali89@gmail.com>
2021-05-10 09:44:44 +0200

    Added App

--- src/main/java/com/atexio/jeudecartes/App.java
+++ src/main/java/com/atexio/jeudecartes/App.java
@@ -1,13 +1,23 @@
 package com.atexio.jeudecartes;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import com.atexio.jeudecartes.domain.Joueur;
+import com.atexio.jeudecartes.domain.Paquet;
+
+public class App
+
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+
+	public static void main(String[] args) {
+
+		Paquet paquet = new Paquet();
+		paquet.initialiserPaquet();
+
+		System.out.println("========== La main non triée ============");
+		Joueur joueur = new Joueur();
+		joueur.tirerMain(paquet);
+
+		System.out.println("==========la main triée =========");
+		joueur.trierMain();
+
+	}
 }
